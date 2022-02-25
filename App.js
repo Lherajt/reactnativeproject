382151d96a87229274960374803ed599da629111
Lherajt <64788512+Lherajt@users.noreply.github.com>
2022-02-25 11:54:49 -0500

    Lesson 6 and 7

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import React, { useState } from 'react'
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from 'react-native';
 
 
 export default function App() {
 
   const [name, setName] = useState('Lukas')
   const [age, setAge] = useState('30')
+  const [people, setPeople] = useState([
+    { name: 'shaun', id: '1' },
+    { name: 'yoshi', id: '2' },
+    { name: 'mario', id: '3' },
+    { name: 'luigi', id: '4' },
+    { name: 'peach', id: '5' },
+    { name: 'toad', id: '6' },
+    { name: 'bowser', id: '7' },
+  ]);
   // const [person, setPerson] = useState({ name: 'naruto', age: 17 })
 
   // const clickHandler = () => {
@@ -16,27 +25,55 @@ export default function App() {
   return (
     <View style={styles.container}>
 
-      <Text>Enter name:</Text>
+
+      {/* FlatList better for performance than ScrollView, Automaticaly finds key */}
+      <FlatList
+        data={people}
+        renderItem={({ item }) => (
+          <Text style={styles.item}>{item.name}</Text>
+        )}
+      />
+
+
+
+      {/* <ScrollView>
+        {people.map(item => (
+          <View key={item.id}>
+            <Text style={styles.item}>{item.name}</Text>
+          </View>
+        )
+
+        )}
+      </ScrollView> */}
+
+
+      {/* <Text>Enter name:</Text>
       <TextInput
         style={styles.input}
         placeholder='e.g. Steve Harvey'
         onChangeText={(val) => setName(val)}
       />
-
       <Text>Enter age:</Text>
       <TextInput
         style={styles.input}
         placeholder='e.g. 22'
         onChangeText={(val) => setAge(val)}
       />
-      <Text>Hello, my name is {name}, age: {age}</Text>
+      <Text>Hello, my name is {name}, age: {age}</Text> */}
+
+
 
       {/* <Text>My ninja hero is {person.name} and he is {person.age} old!!!!!</Text> */}
+
+
 
 
       {/* <View style={styles.buttonContainer}>
         <Button title='update state' onPress={clickHandler} />
       </View> */}
+
+
+
     </View>
   );
 }
@@ -44,18 +81,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 40,
+    paddingHorizontal: 20,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   // buttonContainer: {
   //   marginTop: 20,
   // },
-  input: {
-    borderWidth: 1,
-    borderColor: '#777',
-    padding: 8,
-    margin: 10,
-    width: 200,
-  }
+  // input: {
+  //   borderWidth: 1,
+  //   borderColor: '#777',
+  //   padding: 8,
+  //   margin: 10,
+  //   width: 200,
+  // }
+  item: {
+    // flex: 1,
+    // marginHorizontal: 10,
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: 'pink',
+    fontSize: 24,
+  },
 });
