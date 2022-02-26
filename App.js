b5aa8dbe4204c3221c7a59a098cde7ff20eb90d2
Lherajt <64788512+Lherajt@users.noreply.github.com>
2022-02-26 18:40:07 -0500

    started todo app

--- App.js
+++ App.js
@@ -1,20 +1,85 @@
 import React, { useState } from 'react'
-import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList, TouchableOpacity } from 'react-native';
+import Header from './components/header'
 
 
 export default function App() {
 
-  const [name, setName] = useState('Lukas')
-  const [age, setAge] = useState('30')
-  const [people, setPeople] = useState([
-    { name: 'shaun', id: '1' },
-    { name: 'yoshi', id: '2' },
-    { name: 'mario', id: '3' },
-    { name: 'luigi', id: '4' },
-    { name: 'peach', id: '5' },
-    { name: 'toad', id: '6' },
-    { name: 'bowser', id: '7' },
+  const [todos, setTodos] = useState([
+    { text: 'buy coffee', key: '1' },
+    { text: 'create an app', key: '2' },
+    { text: 'play on the switch', key: '3' }
   ]);
+
+  return (
+    <View style={styles.container}>
+    <Header/>
+    {/* header */}
+      <View style={styles.container}>
+      {/* to form */}
+        <View style={styles.container}>
+          <FlatList
+            data={todos}
+            renderItem={({item}) => (
+              <Text>{item.text}</Text>
+            )}
+          />
+        </View>
+      </View>
+    </View>
+  )
+}
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+    },
+    content: {
+      padding: 40,
+    },
+    list: {
+      marginTop: 20,
+    },
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // const [name, setName] = useState('Lukas')
+  // const [age, setAge] = useState('30')
+  // const [people, setPeople] = useState([
+  //   { name: 'shaun', id: '1' },
+  //   { name: 'yoshi', id: '2' },
+  //   { name: 'mario', id: '3' },
+  //   { name: 'luigi', id: '4' },
+  //   { name: 'peach', id: '5' },
+  //   { name: 'toad', id: '6' },
+  //   { name: 'bowser', id: '7' },
+  // ]);
+
+  // const pressHandler = (id) => {
+  //   console.log(id);
+  //   setPeople((prevPeople) => {
+  //     return prevPeople.filter(person => person.id !== id)
+  //   })
+  // }
   // const [person, setPerson] = useState({ name: 'naruto', age: 17 })
 
   // const clickHandler = () => {
@@ -22,17 +87,20 @@ export default function App() {
   //   setPerson({ name: 'Itachi', age: 19 })
   // }
 
-  return (
-    <View style={styles.container}>
+  // return (
+  //   <View style={styles.container}>
 
 
       {/* FlatList better for performance than ScrollView, Automaticaly finds key */}
-      <FlatList
+      {/* <FlatList
         data={people}
         renderItem={({ item }) => (
-          <Text style={styles.item}>{item.name}</Text>
+          <TouchableOpacity onPress={() => pressHandler(item.id)}>
+
+            <Text style={styles.item}>{item.name}</Text>
+          </TouchableOpacity>
         )}
-      />
+      /> */}
 
 
 
@@ -74,33 +142,33 @@ export default function App() {
 
 
 
-    </View>
-  );
-}
+//     </View>
+//   );
+// }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 40,
-    paddingHorizontal: 20,
-    backgroundColor: '#fff',
-  },
-  // buttonContainer: {
-  //   marginTop: 20,
-  // },
-  // input: {
-  //   borderWidth: 1,
-  //   borderColor: '#777',
-  //   padding: 8,
-  //   margin: 10,
-  //   width: 200,
-  // }
-  item: {
-    // flex: 1,
-    // marginHorizontal: 10,
-    marginTop: 24,
-    padding: 30,
-    backgroundColor: 'pink',
-    fontSize: 24,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     paddingTop: 40,
+//     paddingHorizontal: 20,
+//     backgroundColor: '#fff',
+//   },
+//   // buttonContainer: {
+//   //   marginTop: 20,
+//   // },
+//   // input: {
+//   //   borderWidth: 1,
+//   //   borderColor: '#777',
+//   //   padding: 8,
+//   //   margin: 10,
+//   //   width: 200,
+//   // }
+//   item: {
+//     // flex: 1,
+//     // marginHorizontal: 10,
+//     marginTop: 24,
+//     padding: 30,
+//     backgroundColor: 'pink',
+//     fontSize: 24,
+//   },
+// });
